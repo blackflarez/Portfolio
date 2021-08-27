e66f79a774c73ad67703db02fc0645265bc9752d
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2021-08-28 03:58:32 +1200

    Skeleton website
    Mostly placeholder text, but the design and layout are looking good.

---
+++ RubenGueorguiev/server.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+var path = require('path')
+const app = express()
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.use(express.static('./public'))
+
+app.get('/', (request, response) => {
+  response.render('home')
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Started on port ${port}!`))
