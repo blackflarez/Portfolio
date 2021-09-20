b7ed8df9eece3d30a0c67aeada5826b168d09c48
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2021-09-21 00:38:26 +1200

    Moved files to root folder.
    This might fix deployment issues with Heroku.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-
 const express = require('express')
 var path = require('path')
 const app = express()
 
-
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('./public'))
 
 app.get('/', (request, response) => {
   response.render('home')
+})
+
+app.get('/jobspy', (request, response) => {
+  response.render('jobspy')
 })
 
 const port = 3000
