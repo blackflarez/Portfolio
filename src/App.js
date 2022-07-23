4b61d020c63bf418e359888d16e0bd8d977fba22
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2022-07-23 22:29:10 +1200

    Added glassmorphism

--- src/App.js
+++ src/App.js
@@ -5,34 +5,53 @@ function App() {
     <div className="content">
       <body>
         <header>
-          <h1>
-            Hey, I'm <span className="gradient-text">Ruben Gueorguiev</span>.
-          </h1>
+          <div class="header">
+            <h1 class="title">
+              Hi, I'm Ruben Gueorguiev. <br />
+              <br /> I design and develop apps, websites and games with an
+              emphasis on a great user experience.
+            </h1>
 
-          <div className="card">
-            <h4>
-              <a className="link" href="/">
+            <div className="outline">
+              <a
+                className="link"
+                target="_blank"
+                rel="noreferrer"
+                href="mailto:rubengueorguiev@gmail.com"
+              >
                 Email
               </a>
-            </h4>
-
-            <h4>
-              <a className="link" href="/">
+              <a
+                className="link"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/blackflarez"
+              >
                 GitHub
               </a>
-            </h4>
-
-            <h4>
-              <a className="link" href="/">
+              <a
+                className="link"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/ruben-gueorguiev-1bb0151b8/"
+              >
                 LinkedIn
               </a>
-            </h4>
+              <a
+                className="link"
+                target="_blank"
+                rel="noreferrer"
+                href="RubenGueorguiev.pdf"
+              >
+                CV
+              </a>
+            </div>
           </div>
         </header>
 
         <h2 className="title">Work in progress</h2>
 
-        <a href="/" className="link">
+        <a href="/" className=" card">
           <div>
             <h3>Dasher</h3>
             <p>
@@ -40,9 +59,31 @@ function App() {
               communities around New Zealand.
             </p>
           </div>
+          <h5 class="pill">Mobile Development</h5>
         </a>
 
-        <a href="/" className="link">
+        <a href="/" className=" card">
+          <div>
+            <h3>ProjectPOS</h3>
+            <p>An intiutive Point of Sale for restaurant businesses.</p>
+          </div>
+          <h5 class="pill">Mobile Development</h5>
+        </a>
+
+        <h2 className="title">Completed projects</h2>
+
+        <h3>2022</h3>
+        <a href="/" className=" card">
+          <div>
+            <h3>VxtTest</h3>
+            <p>
+              First place for the Vxt Virtual Competition; automating the
+              testing of Vxt infrastructure in a handy web interface.
+            </p>
+          </div>
+          <h5 class="pill">Testing / Automation</h5>
+        </a>
+        <a href="/" className=" card">
           <div>
             <h3>SmashBlock</h3>
             <p>
@@ -50,44 +91,20 @@ function App() {
               physics.
             </p>
           </div>
+          <h5 class="pill">Game Development</h5>
         </a>
-
-        <h2 className="title">Completed projects</h2>
-
-        <h5 className="pill">2022</h5>
-        <a href="/" className="link">
+        <a href="/" className=" card">
           <div>
-            <h3>VxtTest</h3>
+            <h3>Retro Linktree</h3>
             <p>
-              First place for the Vxt Virtual Competition software category;
-              automating the testing of Vxt infrastructure.
+              A retro terminal landing page website with realistic CRT shaders.
             </p>
           </div>
+          <h5 class="pill">Web Development</h5>
         </a>
 
-        <h5 className="pill">2021</h5>
-
-        <a href="/" className="link">
-          <div>
-            <h3>Software Practice Empirical Evidence Repository</h3>
-            <p>
-              A searchable database with evidence about different claims and
-              different Software Engineering practices.
-            </p>
-          </div>
-        </a>
-
-        <a href="/" className="link">
-          <div>
-            <h3>React Native 3D Model</h3>
-            <p>
-              A fully textured GLTF model viewable on mobile devices, utilising
-              ExpoTHREE.
-            </p>
-          </div>
-        </a>
-
-        <a href="/" className="link">
+        <h3>2021</h3>
+        <a href="/" className=" card">
           <div>
             <h3>JobSpy</h3>
             <p>
@@ -95,14 +112,37 @@ function App() {
               presents them in one simple website.
             </p>
           </div>
+          <h5 class="pill">Fullstack Development</h5>
+        </a>
+        <a href="/" className=" card">
+          <div>
+            <h3>Software Practice Empirical Evidence Repository</h3>
+            <p>
+              A searchable database with evidence about different claims and
+              different Software Engineering practices.
+            </p>
+          </div>
+          <h5 class="pill">Frontend Development</h5>
         </a>
 
-        <h5 className="pill">2020</h5>
-        <a href="/" className="link">
+        <a href="/" className=" card">
+          <div>
+            <h3>React Native 3D Model</h3>
+            <p>
+              A fully textured GLTF model viewable on mobile devices, utilising
+              ExpoTHREE.
+            </p>
+          </div>
+          <h5 class="pill">Mobile Development</h5>
+        </a>
+
+        <h3>2020</h3>
+        <a href="/" className=" card">
           <div>
             <h3>Pettokonpanion</h3>
             <p>A mini Java based game inspired by Tamagotchi.</p>
           </div>
+          <h5 class="pill">Game Development</h5>
         </a>
       </body>
     </div>
